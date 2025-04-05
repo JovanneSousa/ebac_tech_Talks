77637bf482d0a073018c4cba07d1dc6d0e463918
Jovanne Sousa <jovanemaciel943@gmail.com>
2025-04-05 00:43:44 -0400

    commit final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('ola mundo');
+AOS.init();
+
+const dataDoEvento = new Date("jan 01, 2026, 00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24 ;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / (diaEmMs));
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % (diaEmMs) / (horasEmMs));
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % (horasEmMs) / (minutosEmMs));
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutosEmMs / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+
+}, 1000);
